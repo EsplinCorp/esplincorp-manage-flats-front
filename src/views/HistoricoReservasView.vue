1f52afb201f599432061529c48c3f9a65ba6a0e2
Eduardo Esplinio <eduesplinio.dat@gmail.com>
2024-07-27 12:59:24 -0300

    fix(reservation): adjust status`

--- src/views/HistoricoReservasView.vue
+++ src/views/HistoricoReservasView.vue
@@ -228,7 +228,7 @@ export default {
       if (checkInDate <= today && checkOutDate >= today) {
         return { text: "Hospedado", color: "green" };
       } else if (checkInDate > today) {
-        return { text: "Reservada Confirmada", color: "orange" };
+        return { text: "Reserva Confirmada", color: "orange" };
       }
       return { text: "Hospedagem Concluída", color: "grey" };
     },
